a530494c6663a86b45e1f5b212dc9897f68a5055
snowflakecat1225 <flamitchcat1225@gmail.com>
2025-01-24 22:01:33 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace SearchFiles
+{
+    class Program
+    {
+        private static readonly List<string> _searchResults = new();
+
+        private static void SearchFile(string directory, string fileName)
+        {
+            var fullPath = Path.Combine(directory, fileName);
+            
+            if (File.Exists(fullPath))
+                _searchResults.Add(fullPath);
+            else
+            {
+                try
+                {
+                    var dirs = Directory.GetDirectories(directory);
+
+                    if (dirs.Length != 0)
+                        foreach (var dir in dirs)
+                            SearchFile(dir, fileName);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        public static void Main()
+        {
+            SearchFile("/home/" + Environment.UserName, "Full_of_cookies.png"); //to check results of the program
+
+            foreach (var result in _searchResults)
+                Console.WriteLine(result);
+        }
+    }
+}
